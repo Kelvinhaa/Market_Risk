c10efb7461b02d6a823dddb7413bbb85ed4ba681
Kelvin <haminhhoang19@gmail.com>
2026-04-27 23:30:33 +1000

    feat: add cmake list for program compile

--- src/price_tracker.cpp
+++ src/price_tracker.cpp
@@ -3,16 +3,24 @@
 
 
 void price_tracker::on_price(const price_packet& p) {
+    // First time seing this symbol, create a struct
     if (data_.find(p.symbol) == data_.end()) {
         Chart new_data;
         o.open = p.price;
         o.low = p.price;
         o.close = p.price;
+        data_[symbol] = o;
     }
-    Chart& new_data = data_.symbol;
-    
-    new_data.ticks++;
+    // get reference to this symbol
+    Chart& new_data = data_[p.symbol];
 
+    // update every tick
+    o.close = p.price;
+    o.volume += p.volume; 
+    o.ticks++;
+
+    if (p.price > o.high) o.high = p.price;
+    if (p.price < o.low)  o.low  = p.price;
 }
 
 void price_tracker::print_all() {
@@ -26,4 +34,8 @@ void price_tracker::print_all() {
                   << " close: " << o.close
                   << "\n";
     }
+}
+
+const Chart& PriceTracker::get(const std::string& symbol) const {
+    return data_.at(symbol);
 }
